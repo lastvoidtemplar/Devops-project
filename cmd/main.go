c610a93c5e0802a928392e603297e1fa35bc2f0f
Deyan Delchev <deyandelchev03@gmail.com>
2025-02-01 14:11:52 +0200

    fix app bug

--- cmd/main.go
+++ cmd/main.go
@@ -10,32 +10,40 @@ import (
 const HelloWorld = "Hello World"
 const Port = 80
 
+var Hostname string = ""
+
 func HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte(fmt.Sprintf("404 Not Found: %s", r.URL.Path)))
+		return
+	}
+
+	log.Println("Hello World path was hit!")
 	w.Write([]byte(HelloWorld))
 }
 
 func HostnameHandler(w http.ResponseWriter, r *http.Request) {
-	hostname, err := os.Hostname()
-
-	if err != nil {
-		log.Printf("Error occured when getting hostname from the kernel: %s", err.Error())
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("Couldn`t get hostname!"))
-		return
-	}
-
-	w.Write([]byte(hostname))
+	log.Println("Hostname path was hit!")
+	w.Write([]byte(Hostname))
 }
 
 func main() {
+	Hostname = os.Getenv("HOSTNAME")
+
+	if Hostname == "" {
+		log.Fatal("Missing ENV HOSTNAME")
+	}
+
+	log.Printf("Hostname: %s\n", Hostname)
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", HelloWorldHandler)
 	mux.HandleFunc("/hostname", HostnameHandler)
 
-	log.Printf("Server is listening on port %d", Port)
+	log.Printf("Server is listening on port %d\n", Port)
 
 	if err := http.ListenAndServe(fmt.Sprintf(":%d", Port), mux); err != nil {
-		log.Fatalf("Error occured when server was listening on post %d: %s", Port, err.Error())
+		log.Fatalf("Error occured when server was listening on post %d: %s\n", Port, err.Error())
 	}
 }
