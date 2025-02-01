f8c3480a55e6a731c3a3078aeb3db8ecb542f9e4
Deyan Delchev <deyandelchev03@gmail.com>
2025-02-01 13:40:29 +0200

    add ansible playbook

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -46,6 +46,13 @@ resource "aws_security_group" "allow_web_traffic" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = {
     Name = "allow_web_traffic"
   }
